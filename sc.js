dd678d43974bacef26bc9c192942a31478401e7f
mc1c <mario.cichonczyk@gmail.com>
2023-11-13 15:34:46 +0100

    zoom test

--- sc.js
+++ sc.js
@@ -4,3 +4,8 @@ element.addEventListener("click", toggleDoor);
 function toggleDoor() {
   element.classList.toggle("doorOpen");
 }
+
+var zf = Math.floor(document.documentElement.clientWidth/500)
+var zfp = String(100*zf)+"%"
+document.body.style.zoom = zfp
+console.log(zfp)
